105f22cbf5a6ae09cd9420c00f3b9e4e1a70fb58
Rajallinst <104083951+Rajallinst@users.noreply.github.com>
2022-04-22 16:19:12 +0700

    Create while Loop.py

---
+++ while Loop.py
@@ -0,0 +1,9 @@
+print("Program Bilangan Bulat 1 hingga x")
+i = 1;
+x = int(input("Masukkan bilangan bulat x = "));
+
+while i <= x:
+    print(i);
+    i=i+1;
+else:
+    print("bilangan bulat 1 hingga ", i-1, "ditampilkan");
